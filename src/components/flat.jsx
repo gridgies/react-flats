8b8ff40a85b77fdb866133eecf444063089ad287
Graciella Dharmawan <g.dharmawan@student.maastrichtuniversity.nl>
2021-07-06 17:51:38 +0200

    flats showing

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -10,7 +10,6 @@ class Flat extends Component {
   }
 
   render() {
-    console.log(this.props.url);
     const src = `${this.props.imageUrl}`;
     return (
       <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${src})`}}
